9e2bb2adf0a7d0def5783ea4c3bce05844da91a9
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2026-05-03 23:09:54 -0400

    Added compressCSS task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ const {src, watch, dest, series} = require(`gulp`),
     htmlValidator = require(`gulp-html`),
     CSSLinter = require(`gulp-stylelint`),
     jsLinter = require(`gulp-eslint`),
-    htmlCompressor = require(`gulp-htmlmin`);
+    htmlCompressor = require(`gulp-htmlmin`),
+    cssCompressor = require(`gulp-clean-css`);
 
 let validateHTML = () => {
     return src(`index.html`)
@@ -31,7 +32,14 @@ let compressHTML = () => {
         .pipe(dest(`prod`));
 };
 
+let compressCSS = () => {
+    return src(`styles/*.css`)
+    .pipe(cssCompressor())
+    .pipe(dest(`prod/styles`));
+};
+
 exports.validateHTML = validateHTML;
 exports.lintCSS = lintCSS;
 exports.lintJS = lintJS;
 exports.compressHTML = compressHTML;
+exports.compressCSS = compressCSS;
