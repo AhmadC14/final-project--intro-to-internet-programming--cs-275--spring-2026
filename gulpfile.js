40d2cd991a4feb41a629ecce48f1a140e01d3144
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2026-05-03 22:44:40 -0400

    Added lintCSS tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 const {src, watch, dest, series} = require(`gulp`),
-    htmlValidator = require(`gulp-html`);
+    htmlValidator = require(`gulp-html`),
+    CSSLinter = require(`gulp-stylelint`);
 
 let validateHTML = () => {
     return src(`index.html`)
         .pipe(htmlValidator(undefined));
 };
 
+let lintCSS = () => {
+    return src(`styles/*.css`)
+        .pipe(CSSLinter({
+            failAfterError: false,
+            reporters: [
+                {formatter: `string`, console: true}
+            ]
+        }));
+};
+
 exports.validateHTML = validateHTML;
+exports.lintCSS = lintCSS;
