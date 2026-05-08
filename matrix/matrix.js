cb24af2e0e2c270c988d01f7eb20c6914713ded1
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2026-05-08 11:28:01 -0400

    Changed generation to a function

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,7 +1,5 @@
-let matrixSize = 9;
-let numOfSpace = parseInt((matrixSize / 2) - 1);
-let numOfStar = 3;
-let topHalf = true
+let matrixSize,numOfSpace,numOfStar;
+let topHalf = true;
 let matrix = document.createElement(`table`);
 let tableHeader = document.createElement(`thead`);
 let tableBody = document.createElement(`tbody`);
@@ -9,39 +7,44 @@ let tableFooter = document.createElement(`tfoot`);
 let singleStar = document.createElement(`th`);
 let firstRow = document.createElement(`tr`);
 let body = document.querySelector(`body`);
+let button = document.getElementById(`matrix-button`);
 
-singleStar.colSpan = matrixSize;
-singleStar.innerText = `*`;
-firstRow.append(singleStar.cloneNode(true));
-tableHeader.appendChild(firstRow);
-tableFooter.appendChild(firstRow.cloneNode(true));
-matrix.appendChild(tableHeader);
+let generateMatrix = () =>{
+    singleStar.colSpan = matrixSize;
+    singleStar.innerText = `*`;
+    firstRow.append(singleStar.cloneNode(true));
+    tableHeader.appendChild(firstRow);
+    tableFooter.appendChild(firstRow.cloneNode(true));
+    matrix.appendChild(tableHeader);
 
-for(let i = 1; i <= matrixSize - 2; ++i){
-    let row = document.createElement(`tr`);
-    let space = document.createElement(`th`);
-    let star = document.createElement(`th`);
+    for(let i = 1; i <= matrixSize - 2; ++i){
+        let row = document.createElement(`tr`);
+        let space = document.createElement(`th`);
+        let star = document.createElement(`th`);
 
-    star.innerText = `*`;
-    space.innerText = ` `;
+        star.innerText = `*`;
+        space.innerText = ` `;
 
-    space.colSpan = numOfSpace;
+        space.colSpan = numOfSpace;
 
-    if(numOfSpace > 0){
-        row.appendChild(space);
+        if(numOfSpace > 0){
+            row.appendChild(space);
+        }
+
+        row.append(...Array.from({ length: numOfStar }, () => star.cloneNode(true)));
+        tableBody.appendChild(row);
+
+        console.log(`Row:${i} Number of space: ${numOfSpace} Stars: ${numOfStar}\n`);
+
+        numOfSpace = (topHalf)? --numOfSpace : ++numOfSpace;
+        numOfStar = (topHalf) ? numOfStar + 2 : numOfStar - 2;
+
+        topHalf = (i >= parseInt((matrixSize - 2)/2)) ? false : true;
     }
 
-    row.append(...Array.from({ length: numOfStar }, () => star.cloneNode(true)));
-    tableBody.appendChild(row);
-
-    console.log(`Row:${i} Number of space: ${numOfSpace} Stars: ${numOfStar}\n`);
-
-    numOfSpace = (topHalf)? --numOfSpace : ++numOfSpace;
-    numOfStar = (topHalf) ? numOfStar + 2 : numOfStar - 2;
-
-    topHalf = (i >= parseInt((matrixSize - 2)/2)) ? false : true;
+    matrix.append(tableBody);
+    matrix.appendChild(tableFooter);
+    body.append(matrix);
 }
 
-matrix.append(tableBody);
-matrix.appendChild(tableFooter);
-body.append(matrix);
+button.addEventListener(`click`,);
