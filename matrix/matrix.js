5a57960751a2c43b4c60ccf21f4e8f4552aebaac
SloaneSamele <120995022+SloaneSamele@users.noreply.github.com>
2026-05-08 09:57:40 -0400

    Added the first and last row

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,14 +1,19 @@
-let matrixSize = 3;
+let matrixSize = 9;
+let numOfSpace = parseInt((matrixSize / 2) - 1);
+let numOfStar = 3;
+let topHalf = true
 let matrix = document.createElement(`table`);
 let tableHeader = document.createElement(`thead`);
 let tableFooter = document.createElement(`tfoot`);
-let singleStar = document.createElement(`th`);
+let star = document.createElement(`th`);
+let space = document.createElement(`th`);
 let firstRow = document.createElement(`tr`);
 let body = document.querySelector(`body`);
 
-singleStar.colSpan = matrixSize;
-singleStar.innerText = `*`;
-firstRow.append(singleStar);
+star.colSpan = matrixSize;
+star.innerText = `*`;
+space.innerText = ` `;
+firstRow.append(star.cloneNode(true));
 tableHeader.appendChild(firstRow);
 tableFooter.appendChild(firstRow.cloneNode(true));
 matrix.appendChild(tableHeader);
