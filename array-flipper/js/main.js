6d94cf6f1bc79536d6413f21fe561fd7cdb43a5b
Jay Garcia <jaypty153@gmail.com>
2026-05-10 01:44:54 -0400

    Fixed for the Single for loop for the logic part

--- array-flipper/js/main.js
+++ array-flipper/js/main.js
@@ -1,19 +1,33 @@
 const input = window.prompt("Enter a number");
 const number = Number(input);
 const output = document.getElementById('output');
+let totalRows;
 
 if (number % 2 === 0) {
-    for (let row = 1; row <= number + 1; row++) {
-        let stars = '';
-        let spaces = '';
-        let totalStars;
+    totalRows = number + 1;
+}
 
-        if (row === 1 || row === number + 1) {
+else {
+    totalRows = number;
+}
+
+for (let row = 1; row <= totalRows; row++) /* The Single For-Loop Logic*/ {
+
+    let stars = '';
+    let spaces = '';
+    let totalStars;
+
+    if (number % 2 === 0) {
+
+        if (row === 1 || row === totalRows) {
             totalStars = 1;
-        } else if (row <= (number / 2) + 1) {
+        }
+        else if (row <= (number / 2) + 1) {
             totalStars = (row - 1) * 2;
-        } else {
-            totalStars = (number + 1 - row) * 2;
+        }
+
+        else {
+            totalStars = (totalRows - row) * 2;
         }
 
         let totalSpaces = number - totalStars;
@@ -26,33 +40,29 @@ if (number % 2 === 0) {
             stars += '* ';
         }
 
-        output.textContent += `${spaces}${stars}\n`;
     }
-}
 
-else {
-    for (let row = 1; row <= number; row++) {
-        let stars = '';
-        let spaces = '';
-        let totalStars;
-        
+    else {
+
         if (row <= (number + 1) / 2) {
-        totalStars = row * 2 - 1;
+            totalStars = (row * 2) - 1;
         }
-        
+
         else {
-        totalStars = (number - row + 1) * 2 - 1;
+            totalStars = ((number - row + 1) * 2) - 1;
         }
 
         let totalSpaces = ((number + 1) - totalStars) / 2;
-        for (let column = 1; column <= totalSpaces; column++) {
-        spaces += ' ';
+
+        for (let column = 1; column <= totalSpaces; column++) /*Rendering Spaces*/ {
+            spaces += ' ';
         }
 
-        for (let column = 1; column <= totalStars; column++) {
-        stars += '*';
+        for (let column = 1; column <= totalStars; column++) /*Rendering Stars*/ {
+            stars += '*';
         }
-        
-        output.textContent += `${spaces}${stars}\n`;
+
     }
+
+    output.textContent += `${spaces}${stars}\n`;
 }
