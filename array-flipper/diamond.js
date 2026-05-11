b7903febcb19151208b4f67a0d2bfe206fab4955
kaidenton <notnediak@gmail.com>
2026-05-11 13:45:49 -0400

    added/fixed even case for diamond
    i should have added the code originally but literally forgot to put + 1 in line 39

--- array-flipper/diamond.js
+++ array-flipper/diamond.js
@@ -10,7 +10,7 @@ const generateDiamond = (num = 3) => {
   const isEven = num % 2 === 0;
   const half = Math.floor(num / 2);
 
-  for (let row = 0; row < num; row++) {
+  for (let row = 0; row <= num; row++) {
     let spaces = "";
     let pluses = "";
 
@@ -19,7 +19,32 @@ const generateDiamond = (num = 3) => {
       spaces = " ".repeat(distance);
       pluses = "+".repeat(num - 2 * distance);
     } else {
-      //insert code here for even case
+      if (0 === row || 1 === row) {
+        spaces = " ".repeat(num - row);
+        pluses = "+ ".repeat(row + 1).trim();
+      }
+
+      else if (row === num) {
+        spaces = " ".repeat(num);
+        pluses = "+ ";
+      }
+
+      else if (row < half) {
+        let count = row * 2;
+        spaces = " ".repeat(num - 2 * row + 1);
+        pluses = "+ ".repeat(count).trim();
+      }
+
+      else if (row > half) {
+        let bottomRow = num + 1 - row;
+        spaces = " ".repeat(num - 2 * bottomRow + 3);
+        pluses = "+ ".repeat(bottomRow * 2 - 2).trim();
+      }
+
+      else {
+        spaces = " ";
+        pluses = "+ ".repeat(num).trim();
+      }
     }
     result += spaces + pluses + "\n";
   }
