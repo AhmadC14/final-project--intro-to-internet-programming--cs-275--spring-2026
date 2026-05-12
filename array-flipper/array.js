e3011615f81f53249e9da068d56a33d66abd5245
katherinesc <kcorrea10506@gmail.com>
2026-05-11 23:02:37 -0400

    Created else statement for odd diamonds
    This only gives the odd diamonds the correct amount of stars without any spaces.

--- array-flipper/array.js
+++ array-flipper/array.js
@@ -6,4 +6,17 @@ for(let row = 0; row < userSize; row++){
     let stars = 0;
     let spaces = 0;
 
+    if(Math.floor(userSize % 2 == 0)){
+
+    } else {
+        if(row <= Math.floor(userSize / 2)){
+            stars = (row * 2) + 1;
+            console.log("*".repeat(stars));
+        } else {
+            stars = (userSize - row) * 2 - 1;
+            spaces = (userSize + 1) / 2 - row;
+            console.log("*".repeat(stars));
+        }
+    }
+
 }
