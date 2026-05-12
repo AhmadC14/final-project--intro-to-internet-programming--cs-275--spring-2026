f9b40d3fa62f9fb45b8be3fd2bf59832ea3e228c
katherinesc <kcorrea10506@gmail.com>
2026-05-12 14:15:29 -0400

    Created if statement for default matrix

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -16,4 +16,11 @@ for(let row = 0; row < total; row++){
 
     let column = row % size
 
+    if(phase === 0){
+        output += "0 ";
+
+        if (column === size - 1) {
+            console.log(output.trim());
+            output = "";
+        }
 }
