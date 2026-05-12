90b54d432285470fb872f9316db9a477911c58e5
katherinesc <kcorrea10506@gmail.com>
2026-05-12 14:10:44 -0400

    Added space for console view

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,6 +1,7 @@
 let size = parseInt(window.prompt("Enter the size of your matrix:"));
 
 console.log("Your matrix is " + size + " x " + size);
+console.log("");
 
 console.log("Printing matrix with default values:");
 
