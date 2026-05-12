c7ee03ec6b01799ae6194535fba598ae1fb62b47
katherinesc <kcorrea10506@gmail.com>
2026-05-12 13:50:24 -0400

    Printed matrix of zeros

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,3 +1,9 @@
 let size = parseInt(window.prompt("Enter the size of your matrix:"));
 
 console.log("Your matrix is " + size + " x " + size);
+
+console.log("Printing matrix with default values:");
+
+for(let row = 0; row < size; row++){
+    console.log("0 ".repeat(size));
+}
