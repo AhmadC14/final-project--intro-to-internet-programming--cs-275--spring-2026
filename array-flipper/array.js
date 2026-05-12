b083fcf23f77f168b3e86f39c47e3de3a2fa6613
katherinesc <kcorrea10506@gmail.com>
2026-05-12 02:29:11 -0400

    Created middle variable

--- array-flipper/array.js
+++ array-flipper/array.js
@@ -2,12 +2,14 @@ console.log("JS");
 
 let userSize = parseInt(window.prompt("Enter a number: "));
 
+let middle = Math.floor(userSize / 2);
+
 for(let row = 0; row < userSize; row++){
     let stars = 0;
     let spaces = 0;
 
-    if(Math.floor(userSize % 2 == 0)){
-        if(row <= Math.floor(userSize / 2)){
+    if(userSize % 2 == 0){
+        if(row <= middle){
 
             if(row == 0){
                 stars = 1;
@@ -23,17 +25,18 @@ for(let row = 0; row < userSize; row++){
             stars = (userSize - row) * 2;
             // spaces = row - (Math.floor(userSize / 2));
             console.log(" *".repeat(stars));
+                spaces = (middle - row );
         }
 
     } else {
-        if(row <= Math.floor(userSize / 2)){
+        if(row <= middle){
             stars = (row * 2) + 1;
-            spaces = Math.floor(userSize / 2) - row;
+            spaces = middle - row;
             console.log(" ".repeat(spaces) + "*".repeat(stars));
 
         } else {
             stars = (userSize - row) * 2 - 1;
-            spaces = row - (Math.floor(userSize / 2));
+            spaces = row - middle;
             console.log(" ".repeat(spaces) + "*".repeat(stars));
         }
     }
